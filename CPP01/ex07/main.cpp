b7ad9661a17c184372d7a467009e1e3125c31aa3
Perso-alesanto <92.alex.ds@gmail.com>
2021-02-18 17:34:33 +0100

    Push propre

--- CPP01/ex07/main.cpp
+++ CPP01/ex07/main.cpp
@@ -8,42 +8,42 @@
 
 int main(int argc, char **argv)
 {
-    std::ifstream ifs;
-    std::ofstream ofs;
-    std::string filename;
-    std::string tmp;
+	std::ifstream ifs;
+	std::ofstream ofs;
+	std::string filename;
+	std::string tmp;
 
-    if (argc != 4)
-    {
-        std::cerr << ERROR_ARGC << std::endl;
-        return(1);
-    }
-    for(int i = 1; i < 4; i++)
-    {
-        std::string tmp = argv[i];
-        if (!tmp.size())
-        {
-            std::cerr << ERROR_NULL << std::endl;
-            return (1);
-        }
-    }
-    ifs.open(argv[1]);
-    if (ifs.fail())
-    {
-        std::cerr << ERROR_OPEN << std::endl;
-        return (1);
-    }
-    filename = argv[1];
-    filename.replace(filename.size(), 9, ".replace");
-    ofs.open(filename.c_str());
-    filename = argv[2];
-    while (std::getline(ifs, tmp))
-    {
-        while (tmp.find(argv[2]) != std::string::npos)
-            tmp.replace(tmp.find(argv[2]), filename.size(), argv[3]);
-        ofs << tmp << std::endl;
-    }
-    ifs.close();
-    ofs.close();
-    return (0);
+	if (argc != 4)
+	{
+		std::cerr << ERROR_ARGC << std::endl;
+		return(1);
+	}
+	for(int i = 1; i < 4; i++)
+	{
+		std::string tmp = argv[i];
+		if (!tmp.size())
+		{
+			std::cerr << ERROR_NULL << std::endl;
+			return (1);
+		}
+	}
+	ifs.open(argv[1]);
+	if (ifs.fail())
+	{
+		std::cerr << ERROR_OPEN << std::endl;
+		return (1);
+	}
+	filename = argv[1];
+	filename.replace(filename.size(), 9, ".replace");
+	ofs.open(filename.c_str());
+	filename = argv[2];
+	while (std::getline(ifs, tmp))
+	{
+		while (tmp.find(argv[2]) != std::string::npos)
+			tmp.replace(tmp.find(argv[2]), filename.size(), argv[3]);
+		ofs << tmp << std::endl;
+	}
+	ifs.close();
+	ofs.close();
+	return (0);
 }
